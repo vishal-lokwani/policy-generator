cf2c01a732ebb57583b4b848d5488825a4283bb7
vishal <bhuvnesh@autovyn.in>
2024-07-30 10:54:22 +0530

    banner change

--- policy_manager/__manifest__.py
+++ policy_manager/__manifest__.py
@@ -17,8 +17,7 @@
         'views/policy_menu.xml',                
         'views/policy_template.xml',
         'views/policy_view.xml',         
-        'static/description/banner1.gif'         
-        
+        'static/description/banner3.gif'         
     ],
     'assets': {
         'web.assets_backend': [
